6a96ff4e0d0d023799099225084e55d13e81978c
ROHIT922PAL <107602326+ROHIT922PAL@users.noreply.github.com>
2022-08-29 18:24:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
 
+import React from 'react'; // get the React object from the react module
+
+class App extends React.Component {
+  render() {
+    return <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+   
+  }
+}
+
+export default App; // expose the HelloWorld component to other modules
