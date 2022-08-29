5e0ffd1dcac9640fddcc394b53376477b614a402
ROHIT922PAL <107602326+ROHIT922PAL@users.noreply.github.com>
2022-08-29 18:39:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 function App() {
-  return ( <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> );
+  render() {
+    return (<h1> hi</h1>);
   }
- 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(App());
+}
+ReactDOM.render(     
+  <App />,
+  document.getElementById('root')        
+);
+
+
+
 
 
 
